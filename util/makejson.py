c1fa90b4fed24355a5092bb6880870c818e42e5f
Desmond Elliott <des.elliott@gmail.com>
2016-04-07 17:17:39 +0200

    makejson now cleanly supports multiple input files per split

--- util/makejson.py
+++ util/makejson.py
@@ -22,39 +22,54 @@ imgid = 0
 splits = ['train', 'val', 'test']
 
 for split in splits:
-  sentences = open("%s/%s_words" % (args.path, split)).readlines()
-  sentences = [x.replace("\n","") for x in sentences]
+  sentence_files = glob.glob("%s/%s.*" % (args.path, split))
+  sentences = []
+  for fname in sentence_files:
+      f_sentences = open("%s" % (fname)).readlines()
+      f_sentences = [x.replace("\n","") for x in f_sentences]
+      sentences.append(f_sentences)
+
   images = open("%s/%s_images" % (args.path, split)).readlines()
   images = [x.replace("\n","") for x in images]
 
   localidx = 0
-  for sent, image in zip(sentences, images):
+  for idx, image in enumerate(images):
+    local_sentences = []
+    for x in sentences:
+        local_sentences.append(x[idx])
 
-    startx = re.sub(r".jpg","", image)
-  
-    sentidstr = ""
-    localCounter = sent_counter
-    sentidstr += "%d, " % localCounter
-    sentidstr = sentidstr[:-2]
-  
+    # Build the string that holds the sentence ids. This will look like
+    # 1,2,3,4,5, and will be transformed into [%s] below.
+    sent_ids = ""
+    local_counter = sent_counter
+    for s in local_sentences:
+        sent_ids += "%d, " % local_counter
+        local_counter += 1
+    sent_ids = sent_ids[:-2] # Get rid of the trailing ", "
+
     handle.write('{"filename":"%s", ' % image)
-    handle.write('"sentids":[%s], ' % sentidstr)
     handle.write('"imgid":%d, ' % imgid)
-    handle.write('"split":"%s", ' % split)
     handle.write('"sentences":[')
-  
-    scontent = sent.split(" ")
-    tokens_str = ''
-    for w in scontent:
-      tokens_str += '"%s", ' % w.replace('"','')
-    tokens_str = tokens_str[:-2]
-      
-    handle.write('{"tokens":[%s], ' % tokens_str)
-    handle.write('"raw":"%s .", ' % sent)
-    handle.write('"imgid":%d, ' % imgid)
-    handle.write('"sentid": %d}]}' % sent_counter)
-    sent_counter += 1
- 
+
+    for sidx, s in enumerate(local_sentences):
+        split_sent = s.split(" ")
+        tokens_str = ''
+        for w in split_sent:
+          tokens_str += '"%s", ' % w.replace('"','')
+        tokens_str = tokens_str[:-2]
+
+        handle.write('{"tokens":[%s], ' % tokens_str)
+        s = s.replace('"', '') # BE CAREFUL, WE ARE THROWING AWAY SPEECH MARKS
+        handle.write('"raw":"%s", ' % s)
+        handle.write('"imgid":%d, ' % imgid)
+        handle.write('"sentid": %d} ' % sent_counter)
+        if sidx < len(local_sentences)-1:
+            handle.write(", ")
+        sent_counter += 1
+    handle.write("], ")
+    handle.write('"split":"%s", ' % split)
+    handle.write('"sentids":[%s]}' % sent_ids)
+
     if split == "test":
       if localidx < len(images)-1:
         handle.write(", ")
@@ -62,7 +77,7 @@ for split in splits:
         continue
     else:
       handle.write(", ")
-  
+
     imgid +=1
     localidx += 1
 
